39470bfe94306be0a2d38f7bfccbd6e88ebe4db3
Eugene <70693118+MrLutik@users.noreply.github.com>
2021-10-06 16:41:38 +0300

    [testnet-5]

--- fnnetcheck.sh
+++ fnnetcheck.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-echo "Sentry container"
-docker exec -it sentry netstat -nptw | grep -E '$(hostanme -I | awk "{print $1}")|26656|36656|56656' | grep ESTABLISHED 
-
-echo "Priv_sentry container" 
-docker exec -it priv_sentry netstat -nptw | grep -E '$(hostanme -I | awk "{print $1}")|26656|36656|56656' | grep ESTABLISHED 
+#validator node and host connection check
 
 echo "Validator container" 
 docker exec -it validator netstat -nptw | grep -E '$(hostanme -I | awk "{print $1}")|26656|36656|56656' | grep ESTABLISHED
