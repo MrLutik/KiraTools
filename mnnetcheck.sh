919f118f2d026eeb6461f3d99d9e6dc2901ac488
Eugene <70693118+MrLutik@users.noreply.github.com>
2021-10-06 16:42:27 +0300

    [testnet-5] upd

--- mnnetcheck.sh
+++ mnnetcheck.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#sentry node and host connection check
 
-echo "Validator container" 
-docker exec -it validator netstat -nptw | grep -E '$(hostanme -I | awk "{print $1}")|26656|36656|56656' | grep ESTABLISHED
+echo "Sentry container" 
+docker exec -it sentry netstat -nptw | grep -E '$(hostanme -I | awk "{print $1}")|26656|36656|56656' | grep ESTABLISHED
 
 echo "Host" 
 netstat -nptw | grep -E '$(hostanme -I | awk "{print $1}")|26656|36656|56656' | grep ESTABLISHED
